6ad0b486e1f65e6d56afe99094311bcb0d545395
mikrukovd <z123456689z@gmail.com>
2025-11-10 12:09:04 +0300

    setup admin UI for likes

--- property/models.py
+++ property/models.py
@@ -7,6 +7,12 @@ class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField('Новостройка', null=True, blank=True)
+    liked_by = models.ManyToManyField(
+        User,
+        verbose_name='Кто лайкнул',
+        blank=True,
+        null=True,
+    )
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
